459d899b29fc281e825f28c9102e0fd5112036dd
cmgoersch <goersch@madways.de>
2024-02-26 15:51:08 +0100

    batch syled

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('Das Dokument ist vollständig geladen.');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Zugriff auf das Eingabefeld und das Ziel-div
+    var inputField = document.getElementById('input');
+    var batchDiv = document.querySelector('.batch');
+
+    // Event-Listener, der auf Eingaben reagiert
+    inputField.addEventListener('input', function() {
+        // Aktualisiert den Inhalt des Ziel-divs mit dem, was im Eingabefeld steht
+        batchDiv.textContent = this.value;
+    });
+});
